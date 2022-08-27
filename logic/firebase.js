7f3b8a1df5595aeedaa141bbc6c799c6dc7b69c5
Eduardo Neves <105514713+NeveSMELO2003@users.noreply.github.com>
2022-08-27 10:37:15 -0300

    To-Do-Neves

---
+++ logic/firebase.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+        apiKey: "AIzaSyB8wHM4tW1RH-35EMJyw5m6kCgLiRRoZBo",
+        authDomain: "to-do-list-eduardo.firebaseapp.com",
+        databaseURL: "https://to-do-list-eduardo-default-rtdb.firebaseio.com",
+        projectId: "to-do-list-eduardo",
+        storageBucket: "to-do-list-eduardo.appspot.com",
+        messagingSenderId: "998087977419",
+        appId: "1:998087977419:web:bc75b7ac80bbcb40f26e4e"
+      };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
